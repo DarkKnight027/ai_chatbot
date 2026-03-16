e385e3db9a3d411a0bcd8d2643193afc1a365505
User <user@example.com>
2026-03-16 09:36:46 +0530

    Add project files

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "PrepAI",
+  description: "An AI-powered chatbot that helps you prepare for your interviews. It provides you with a personalized learning experience, tailored to your needs and goals.  ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monaSans.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
